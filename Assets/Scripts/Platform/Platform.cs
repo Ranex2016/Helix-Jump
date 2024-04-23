75c74d846d345bbfa0b65d33fa04d1ed3651471d
Roma <sevainor@gmail.com>
2024-04-23 13:04:58 +1000

    First commit

---
+++ Assets/Scripts/Platform/Platform.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    [SerializeField] private float bounceForce;
+    [SerializeField] private float bounceRadius;
+
+    public void Break()
+    {
+        PlatformSegment[] platformSegments = GetComponentsInChildren<PlatformSegment>();
+        foreach(var segment in platformSegments)
+        {
+            segment.Bounce(bounceForce, transform.position, bounceRadius);
+            Destroy(segment.gameObject, 8f);
+        }
+    }
+}
---
+++ Assets/Scripts/Tower/TowerBuilder.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBuilder : MonoBehaviour
+{
+    [SerializeField] private float _levelCount;
+    [SerializeField] private GameObject _beam;
+    [SerializeField] private SpawnPlatform spawnPlatform;
+    [SerializeField] private Platform[] platforms;
+    [SerializeField] private FinishPlatform finishPlatform;
+
+    private float aditionalScale = 3;
+
+    private float beamScaleY; 
+    private void Awake()
+    {
+        beamScaleY = _levelCount / 1.5f + 0.5f + aditionalScale / 1.5f;
+        Build();
+    }
+
+    private void Build()
+    {
+        GameObject beam = Instantiate(_beam, transform);
+        beam.transform.localScale = new Vector3(1, beamScaleY, 1);
+        Vector3 spawnPosition = beam.transform.position;
+        spawnPosition.y += beam.transform.localScale.y - aditionalScale;
+
+        SpawnPlatform(spawnPlatform, ref spawnPosition, beam.transform);
+
+        for (int i = 0; i < _levelCount; i++)
+        {
+            SpawnPlatform(platforms[Random.Range(0, platforms.Length)], ref spawnPosition, beam.transform);
+        }
+
+        SpawnPlatform(finishPlatform, ref spawnPosition, beam.transform);
+    }
+
+    private void SpawnPlatform(Platform platform, ref Vector3 spawnPosition, Transform parent)
+    {
+        Instantiate(platform, spawnPosition, Quaternion.Euler(0, Random.Range(0, 360), 0), parent);
+        spawnPosition.y -=1.5f;
+    }
+}
